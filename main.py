72946aafae3097ab31ad19bd13bd009bb6f8ec30
b040213 <b040213@gmail.com>
2025-07-28 20:04:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+import requests
+
+def run_all_tasks():
+    print("這裡寫你抓幣＋判斷＋推 Discord 的邏輯")
+
+while True:
+    run_all_tasks()
+    print("等 25 分鐘再跑")
+    time.sleep(1500)  # 1500秒 = 25分鐘
